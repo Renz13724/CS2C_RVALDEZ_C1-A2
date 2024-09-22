50f22f3093734a7b3b78226de20ad60d2e83c5de
VALDEZ R. <renzv753@gmail.com>
2024-09-22 22:07:53 +0800

    Create LA2_ VALDEZ R_CS2C.js

---
+++ LA2_ VALDEZ R_CS2C.js
@@ -0,0 +1,21 @@
+let a = 25;
+let b = 20;
+let c = 12;
+let d = "15";
+let e = 15;
+let sumOfAll = a + b + c + d + e;
+
+console.log(d > e);
+console.log(d < e);
+console.log(d => e);
+console.log(d <= e);
+console.log(d === e);
+
+let sub_result = a - b - c - (d);
+console.log(sub_result);
+let mul_result = (a * c) / e;
+console.log(mul_result);
+let exp_result = e ** c;
+console.log(exp_result);
+e = c ** 3;
+console.log(e);
